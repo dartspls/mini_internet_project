f8c972efa0e52ca6d3eb740201c613d7d68cf38e
Richard Sanger <rsangerarj@gmail.com>
2021-11-05 12:53:51 +1300

    Add a starting port offset to the portforwarding script
    This makes it easy to configure a different external port range

--- platform/portforwarding.sh
+++ platform/portforwarding.sh
@@ -8,6 +8,11 @@
 #   AllowTcpForwarding yes
 # then restart ssh: service ssh restart
 
+# Starting from this port, map each group's ssh port
+# e.g. With an offset of 2000, G1 receives port 2001
+# and the measurement host 2099.
+PORT_OFFSET=2000
+
 DIRECTORY=$(cd `dirname $0` && pwd)
 source "${DIRECTORY}"/config/subnet_config.sh
 
@@ -21,19 +26,19 @@ for ((k=0;k<group_numbers;k++)); do
 
     if [ "${group_as}" != "IXP" ];then
         if command -v ufw > /dev/null 2>&1; then
-            ufw allow "$((group_number+2000))"
+            ufw allow "$((group_number+PORT_OFFSET))"
         fi
         subnet=$(subnet_ext_sshContainer "${group_number}" "sshContainer")
-        ssh -i groups/id_rsa -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking no" -f -N -L 0.0.0.0:"$((group_number+2000))":"${subnet%/*}":22 root@${subnet%/*}
+        ssh -i groups/id_rsa -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking no" -f -N -L 0.0.0.0:"$((group_number+PORT_OFFSET))":"${subnet%/*}":22 root@${subnet%/*}
     fi
 done
 
 # measurement
 if command -v ufw > /dev/null 2>&1; then
-    ufw allow 2099
+    ufw allow $((99+PORT_OFFSET))
 fi
 subnet=$(subnet_ext_sshContainer "${group_number}" "MEASUREMENT")
-ssh -i groups/id_rsa -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking no" -f -N -L 0.0.0.0:2099:"${subnet%/*}":22 root@${subnet%/*}
+ssh -i groups/id_rsa -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking no" -f -N -L 0.0.0.0:$((99+PORT_OFFSET)):"${subnet%/*}":22 root@${subnet%/*}
 
 
 # for pid in $(ps aux | grep ssh | grep StrictHostKeyChecking | tr -s ' ' | cut -f 2 -d ' '); do sudo kill -9 $pid; done
