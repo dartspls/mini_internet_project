3f8fdee27b1a39104057e4dbc12ad84436ba9fab
Richard Sanger <rsangerarj@gmail.com>
2021-11-09 12:02:11 +1300

    Update docker images for 304
    Updates for the customer docker images used in COMPX304.
    
    * Images build on top of the existing images where possible.
    * Adds some extra packages that students requested
    * Use FRR 7.5.1 on the router

--- platform/docker_images/build_304.sh
+++ platform/docker_images/build_304.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-#
-# build all images and upload to the docker hub
+# Builds customised versions of images for COMPX304.
+# Images are saved as d_name_304 to differentiate them
+# from the standard images
 
 set -o errexit
 set -o pipefail
 set -o nounset
 
-#docker login
-
-# If you want to use your custom docker containers and upload them into
-# docker hub, change the docker username with your own docker username.
-docker_name=thomahol
-
-for name in router ixp host ssh measurement dns switch matrix vpn vlc hostm host_304 switch_304
+for name in ssh_304 host_304 switch_304 router_304
 do
-
 	docker build --tag=d_${name} docker_images/${name}/
-	container_name=d_${name}
-	docker tag "${container_name}" "${docker_name}"/"${container_name}"
 done
