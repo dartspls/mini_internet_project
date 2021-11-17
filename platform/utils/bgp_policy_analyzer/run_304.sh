7a2735c204dcd36197b2842e03340b8efff38d3b
Richard Sanger <rsangerarj@gmail.com>
2021-11-17 16:09:10 +1300

    Add script for bgp_policy_analyzer upload to 304 webserver

---
+++ platform/utils/bgp_policy_analyzer/run_304.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+WEBROOT=/var/www/html/
+STD_USER=rsanger
+
+rm as.db || true
+sudo -u ${STD_USER} python3 cfparse.py ../../config/
+while true
+do
+    sudo -u ${STD_USER} python3 lgparse.py ../../groups/
+    sudo -u ${STD_USER} python3 lganalyze.py print-html | sudo -u ${STD_USER} tee analysis.html > /dev/null
+
+    mkdir -p ${WEBROOT}/bgp_analyzer/
+    cp ./analysis.html ${WEBROOT}/bgp_analyzer/
+
+    echo 'html file sent'
+    sleep 120
+done
